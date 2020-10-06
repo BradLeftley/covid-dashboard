51d8594ce40943eebeeb3ece57263cc8f15068b8
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2020-10-06 21:40:16 +0100

    Fitler by country

--- app/routes/application.js
+++ app/routes/application.js
@@ -6,6 +6,14 @@ export default class ApplicationRoute extends Route {
         let response = await fetch("https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation&structure=%7B%22date%22:%22date%22,%22areaName%22:%22areaName%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22%7D");
         let data = await response.json();
         console.log(data)
-        return data;
+
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        // today = mm + '/' + dd + '/' + yyyy;
+        today = yyyy +'-'+mm+'-'+dd;
+        return data.data.filter(value => value.date === today);
       }
 }
