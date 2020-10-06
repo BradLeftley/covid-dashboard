acd3d90219e64fd94667461db4965d2281fca1ab
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2020-10-06 21:16:01 +0100

    Add country dates

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,8 +3,9 @@ import fetch from "fetch";
 
 export default class ApplicationRoute extends Route {
     async model() {
-        let response = await fetch("https://api.coronavirus.data.gov.uk/v1/data?filters=areaName=United%2520Kingdom;areaType=overview&latestBy=newCasesByPublishDate&structure=%7B%22date%22:%22date%22,%22value%22:%22newCasesByPublishDate%22%7D");
+        let response = await fetch("https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation&structure=%7B%22date%22:%22date%22,%22areaName%22:%22areaName%22,%22newCasesByPublishDate%22:%22newCasesByPublishDate%22%7D");
         let data = await response.json();
+        console.log(data)
         return data;
       }
 }
